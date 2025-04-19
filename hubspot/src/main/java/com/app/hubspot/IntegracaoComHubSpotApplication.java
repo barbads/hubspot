de545054b2de1d80170d0c13d92e9f10d3e8cc32
barbads <matheusbmiranda@gmail.com>
2025-04-19 09:43:23 -0300

    first commit

---
+++ hubspot/src/main/java/com/app/hubspot/IntegracaoComHubSpotApplication.java
@@ -0,0 +1,13 @@
+package com.app.hubspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoComHubSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoComHubSpotApplication.class, args);
+	}
+
+}
---
+++ hubspot/src/test/java/com/app/hubspot/IntegracaoComHubSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hubspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoComHubSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
